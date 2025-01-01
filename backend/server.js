6cbaafd285de32d2731b094eee67caea8943fb63
adhil <adhitmkm@gmail.com>
2025-01-02 03:19:47 +0530

    backend and frontend folder and file structure created, first commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+// const cors = require('cors')
+// const mongoose = require(mongoose)
+// require ('dotenv').config()
+const PORT = 8000
+
+app.listen(PORT,()=>{
+    console.log(`i am reAdy at port ${PORT}`)
+})
