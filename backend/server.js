bce6c7d5ccae2e6c5a54b9910abbc1f2aa99388c
adhil <adhitmkm@gmail.com>
2025-01-02 03:23:24 +0530

    change my port 8000 to 5000

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express()
 // const cors = require('cors')
 // const mongoose = require(mongoose)
 // require ('dotenv').config()
-const PORT = 8000
+const PORT = 5000
 
 app.listen(PORT,()=>{
     console.log(`i am reAdy at port ${PORT}`)
