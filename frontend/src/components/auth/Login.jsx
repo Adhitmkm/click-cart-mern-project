f73afb78d9b18d5c2f0dabb8df43aff040fd4882
adhil <adhitmkm@gmail.com>
2025-01-04 06:12:20 +0530

    axios added

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./auth.css";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const Login = () => {
   const [input, setInput] = useState({
@@ -10,14 +11,35 @@ const Login = () => {
 
   const handleSubmitEvent = (e) => {
     e.preventDefault();
-    if (input.email.trim() == "" && input.password.trim == "")
-      alert("please provide valid input");
-      setInput({
-        email: "",
-        password: "",
-      });
 
+    axios.post('http://localhost:9000/auth/login', { input: 'example data' })
+
+  .then(response => {
+
+    console.log(response.data); // Handle the response data
+
+  })
+
+  .catch(error => {
+
+    console.error('Error:', error); // Handle any errors
+
+  });
+    
+    if (input.email.trim() == "" && input.password.trim == ""){
+
+        alert("please provide valid input");
+    }
+
+        // console.log(input,'values'
+        setInput({
+            email: "",
+            password: "",
+          });
+        //   console.log(input)
   };
+
+
 
   const handleInput = (e) => {
     const { name, value } = e.target;
@@ -29,16 +51,16 @@ const Login = () => {
 
   return (
     <>
-      <div className="login-signup" id="login">
-        <div className="card">
+      <div className="login-signup" >
+        <div className="card" id="login">
         <h1>CLICK CART</h1>
         <h4>Welcome Back!</h4>
         <form onSubmit={handleSubmitEvent} className="form-auth">
             <label>Email</label>
           <div>
-            <input
+            <input type="email"
               name="email"
-            //   value={input.email}
+              value={input.email}
               placeholder="firstname"
               onChange={handleInput}
             />
@@ -46,9 +68,9 @@ const Login = () => {
 
             <label>Password</label>
           <div>
-            <input
+            <input type="password"
               name="password"
-            //   value={input.password}
+              value={input.password}
               placeholder="Password"
               onChange={handleInput}
             />
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './auth.css'
 import { useState } from "react";
-// import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const Signup = ()=> {
     const [input, setInput]=useState({
@@ -15,8 +15,23 @@ const Signup = ()=> {
     const handleSubmitEvent =(e)=>{
         e.preventDefault()
 
+
+        axios.post('http://localhost:9000/auth/signup', { input: 'example data' })
+
+        .then(response => {
+      
+          console.log(response.data); // Handle the response data
+      
+        })
+      
+        .catch(error => {
+      
+          console.error('Error:', error); // Handle any errors
+      
+        });
+
         if(input.firstName.trim() =="" && input.lastName.trim() =="" && input.email.trim() =="" && input.password.trim() =="" && input.conformPassword.trim() =="" ){
-            console.log(input,"valuess1");
+            // console.log(input,"valuess1");
             
           alert("please provide a valid input");
           return
@@ -29,12 +44,12 @@ const Signup = ()=> {
             email: "",
             password: "",
             conformPassword: ""})
-        console.log(input);
+        // console.log(input);
         
         
     }
 
-    console.log(input,"last");
+    // console.log(input,"last");
     
 
     const handleInput = (e) => {
